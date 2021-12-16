9740ea40b19f76f35ef7da836712564ff8adffc3
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-12-16 17:02:49 -0500

    Reduce to one crate

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ impl Trait for S0 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -44,7 +44,7 @@ impl Trait for S1 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -63,7 +63,7 @@ impl Trait for S2 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -82,7 +82,7 @@ impl Trait for S3 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -101,7 +101,7 @@ impl Trait for S4 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -120,7 +120,7 @@ impl Trait for S5 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -139,7 +139,7 @@ impl Trait for S6 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -158,7 +158,7 @@ impl Trait for S7 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -177,7 +177,7 @@ impl Trait for S8 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -196,7 +196,7 @@ impl Trait for S9 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -215,7 +215,7 @@ impl Trait for S10 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -234,7 +234,7 @@ impl Trait for S11 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -253,7 +253,7 @@ impl Trait for S12 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -272,7 +272,7 @@ impl Trait for S13 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -291,7 +291,7 @@ impl Trait for S14 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -310,7 +310,7 @@ impl Trait for S15 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -329,7 +329,7 @@ impl Trait for S16 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -348,7 +348,7 @@ impl Trait for S17 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -367,7 +367,7 @@ impl Trait for S18 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -386,7 +386,7 @@ impl Trait for S19 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -405,7 +405,7 @@ impl Trait for S20 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -424,7 +424,7 @@ impl Trait for S21 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -443,7 +443,7 @@ impl Trait for S22 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -462,7 +462,7 @@ impl Trait for S23 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -481,7 +481,7 @@ impl Trait for S24 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -500,7 +500,7 @@ impl Trait for S25 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -519,7 +519,7 @@ impl Trait for S26 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -538,7 +538,7 @@ impl Trait for S27 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -557,7 +557,7 @@ impl Trait for S28 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
@@ -576,9 +576,355 @@ impl Trait for S29 {
         Box::pin(async move {
             let __self = self;
             let _: () = {
-                helper_crate::entry_point().await;
+                entry_point().await;
             };
         })
     }
 }
 fn main() {}
+
+pub async fn entry_point() {
+    let x = X23::new();
+    x.f().await;
+}
+
+async fn term_async() -> u32 {
+    1
+}
+
+fn term_sync() -> u32 {
+    1
+}
+
+struct X0 {}
+impl X0 {
+    fn new() -> Self {
+        Self {}
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + term_async().await
+    }
+}
+struct X1 {
+    x: X0,
+}
+impl X1 {
+    fn new() -> Self {
+        Self {
+            x: X0::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
+struct X2 {
+    x: X1,
+}
+impl X2 {
+    fn new() -> Self {
+        Self {
+            x: X1::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
+struct X3 {
+    x: X2,
+}
+impl X3 {
+    fn new() -> Self {
+        Self {
+            x: X2::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
+struct X4 {
+    x: X3,
+}
+impl X4 {
+    fn new() -> Self {
+        Self {
+            x: X3::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
+struct X5 {
+    x: X4,
+}
+impl X5 {
+    fn new() -> Self {
+        Self {
+            x: X4::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
+struct X6 {
+    x: X5,
+}
+impl X6 {
+    fn new() -> Self {
+        Self {
+            x: X5::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
+struct X7 {
+    x: X6,
+}
+impl X7 {
+    fn new() -> Self {
+        Self {
+            x: X6::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
+struct X8 {
+    x: X7,
+}
+impl X8 {
+    fn new() -> Self {
+        Self {
+            x: X7::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
+struct X9 {
+    x: X8,
+}
+impl X9 {
+    fn new() -> Self {
+        Self {
+            x: X8::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
+struct X10 {
+    x: X9,
+}
+impl X10 {
+    fn new() -> Self {
+        Self {
+            x: X9::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
+struct X11 {
+    x: X10,
+}
+impl X11 {
+    fn new() -> Self {
+        Self {
+            x: X10::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
+struct X12 {
+    x: X11,
+}
+impl X12 {
+    fn new() -> Self {
+        Self {
+            x: X11::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
+struct X13 {
+    x: X12,
+}
+impl X13 {
+    fn new() -> Self {
+        Self {
+            x: X12::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
+struct X14 {
+    x: X13,
+}
+impl X14 {
+    fn new() -> Self {
+        Self {
+            x: X13::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
+struct X15 {
+    x: X14,
+}
+impl X15 {
+    fn new() -> Self {
+        Self {
+            x: X14::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
+struct X16 {
+    x: X15,
+}
+impl X16 {
+    fn new() -> Self {
+        Self {
+            x: X15::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
+struct X17 {
+    x: X16,
+}
+impl X17 {
+    fn new() -> Self {
+        Self {
+            x: X16::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
+struct X18 {
+    x: X17,
+}
+impl X18 {
+    fn new() -> Self {
+        Self {
+            x: X17::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
+struct X19 {
+    x: X18,
+}
+impl X19 {
+    fn new() -> Self {
+        Self {
+            x: X18::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
+struct X20 {
+    x: X19,
+}
+impl X20 {
+    fn new() -> Self {
+        Self {
+            x: X19::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
+struct X21 {
+    x: X20,
+}
+impl X21 {
+    fn new() -> Self {
+        Self {
+            x: X20::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
+struct X22 {
+    x: X21,
+}
+impl X22 {
+    fn new() -> Self {
+        Self {
+            x: X21::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
+struct X23 {
+    x: X22,
+}
+impl X23 {
+    fn new() -> Self {
+        Self {
+            x: X22::new(),
+        }
+    }
+
+    async fn f(&self) -> u32 {
+        term_sync() + self.x.f().await
+    }
+}
